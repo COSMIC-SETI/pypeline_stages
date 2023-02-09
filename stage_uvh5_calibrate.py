171d5e6ef19169bf976968680a9534e8485f135c
radonnachie <code@radonn.co.za>
2023-02-09 11:38:16 +0000

    * uvh5_calibrate full python3 path

--- stage_uvh5_calibrate.py
+++ stage_uvh5_calibrate.py
@@ -29,7 +29,7 @@ def run(argstr, inputs, env, logger=None):
     # )
     # args = parser.parse_args(argstr.split(" "))
 
-    cmd = f"python3 calibrate_uvh5.py -d {inputs[0]} {argstr}"
+    cmd = f"/home/cosmic/anaconda3/envs/cosmic_vla/bin/python3 calibrate_uvh5.py -d {inputs[0]} {argstr}"
 
     env_base = os.environ.copy()
     env_base.update(common.env_str_to_dict(env))
@@ -54,6 +54,6 @@ if __name__ == "__main__":
         run(
             "-o ./ --genphase --pub-to-redis",
             ["/mnt/buf0/uvh5_commensal/uvh5/uvh5_59949_05980_51052330932_J0303+4716_0001.uvh5"],
-            "CONDA_PYTHON_EXE:/home/svarghes/anaconda3/envs/cosmic_cal/bin/python3"
+            ""
         )
     )
